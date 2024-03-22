3ce03ae46a5f55ae0177f8d06d39c8a1abca4dcd
ozodbekjumaev <ozodjon6@gmail.com>
2024-03-22 15:28:10 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+/** @format */
+
+const data = require("./db.js");
+
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
